2bb4fcc425b8cf9a577a18f888002bbdc1bc7707
b-ulak002 <36213365+b-ulak002@users.noreply.github.com>
2018-06-11 21:41:20 -0400

    Add project files.

---
+++ Sort/SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class SelectionSort
+    {
+        public static void Selection(ref int[] x)
+        {
+            int i, j;
+            int min, temp;
+
+            for (i = 0; i < x.Length - 1; i++)
+            {
+                min = i;
+                for (j = i + 1; j < x.Length; j++)
+                {
+                    if (x[j] < x[min])
+                    {
+                        min = j;
+                    }
+                }
+                temp = x[i];
+                x[i] = x[min];
+                x[min] = temp;
+            }
+        }
+    }   
+}
---
+++ Sort/SortApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class SortApp
+    {
+        static void Main()
+        {
+            //Example
+            MergeSorting ms = new MergeSorting();
+            int[] array = new int[] { 9, 1, 3, 2, 4, 7, 8 };
+            ms.MergeSort(ref array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
